39d8c011d3140b3053a72eea8e003da718ba5b04
Inês Teixeira De Matos <fc43538@alunos.fc.ul.pt>
2020-06-03 17:34:34 +0100

    example

--- example.js
+++ example.js
@@ -2,8 +2,8 @@ var convertemoji = require('emoji-totext');
 
 //var str = "I love you 💙";
 //var str = "I want to eat🍕🍕";
-//var str = "🙍🏾🍕🙆🏾";
-var str = "A🐧 am I !"
+var str = "🙍🏾🍕🙆🏾";
+//var str = "A🐧 am I !"
 
 convertemoji.toText(str,function(err,sentence){
 	console.log(sentence);
